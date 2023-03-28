40de9a0e66fc9cfa6936902c6c4e8bd43eb9f267
Dmitry Safin <developersafin@gmail.com>
2023-03-28 18:34:29 +0500

    Initial Commit

---
+++ BecomeMillionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BecomeMillionaire
+//
+//  Created by Дмитрий on 28.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
